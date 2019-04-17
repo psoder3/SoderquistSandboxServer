3a93925cd4ddf103c51f240d806fda6f081f99a9
pinapple2543 <44168997+pinapple2543@users.noreply.github.com>
2019-04-17 09:37:36 -0600

    Update server.js

--- Back End/server.js
+++ Back End/server.js
@@ -132,7 +132,7 @@ this.motion;
 app.use(myParser.urlencoded({extended : true}));
 app.post('/Potition/gridsend', function(req,res) {
 	console.log(req.body);
-	var data = JSON.parse(req.body);
+	var data = req.body;
 	var inlist = false;
 	
 	for(var i = 0; i < Listofchange.length; i++){
