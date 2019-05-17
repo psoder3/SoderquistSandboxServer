131c3ccf8ba098108102ab3a9700f0a08b1c44a8
tycoonman2144 <43999884+tycoonman2144@users.noreply.github.com>
2019-05-17 09:59:45 -0600

    Update server.js

--- Back End/server.js
+++ Back End/server.js
@@ -227,8 +227,8 @@ app.post('/mozz/gridsend', function(req,res) {
 
         }
 	for(var i = 0; Listofcheese.length < 100; i++){
-		var RandomX = Math.floor((Math.random() * 1900))
-		var RandomY = Math.floor((Math.random() * 1900))
+		var RandomX = Math.floor((Math.random() * 2000)) - 1000
+		var RandomY = Math.floor((Math.random() * 2000)) - 1000
 		var randomcheese = Math.floor((Math.random() * cheeseImglist.length));
 	   	var cheeseobj = {
 			'Id':globalid,
